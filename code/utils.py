02c0f9ab31d753e533ea15382846a30d83545b03
roger <roger.leitzke@gmail.com>
2019-11-13 12:30:03 -0300

    Created function to create pathfiles

--- code/utils.py
+++ code/utils.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-from os.path import exists
+from os.path import exists, join
 import lis
 import progressbar
 
@@ -32,4 +32,20 @@ def check_labels(fileinput):
             dic[flis.obj] = ''
             pb.update()
     logger.info('{}'.format(sorted(dic)))
-    logger.info('Dictionary containing {} labels'.format(len(dic.keys()))
+    logger.info('Dictionary containing {} labels'.format(len(dic.keys())))
+
+
+def create_pathfile(root_folder, output=None):
+    """ Receive the ROOT folder of KSCGR dataset to create
+        a `paths.txt` file containing all paths to images """
+    if not output:
+        output = join(root_folder, 'paths.txt')
+
+    with open(output, 'w') as fout:
+        nb_files = 0
+        for folder, _, files in os.walk(root_folder):
+            for f in files:
+                fout.write('%s\n' % join(folder, f))
+                nb_files += 1
+    logger.info('Create file containing %d paths' % nb_files)
+    logger.info('File saved at: %s' % output)
