3aa1aa5c3b7b3ea127e58d8e5e1b8e9d7af02523
roger <roger.leitzke@gmail.com>
2019-11-29 18:53:45 -0300

    Compress the file of relations using filehandler

--- code/compress_relations.py
+++ code/compress_relations.py
@@ -39,65 +39,37 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-import lis
-import progressbar
-import utils
+import filehandler as fh
 
-def load_relations(inputfile):
-    dic = {}
-    start_frames = []
-    with open(inputfile) as fin:
-        for i, line in enumerate(fin):
-            if line.startswith('Frame'):
-                continue # skip header
-            idf, sub, rel, obj = line.strip().split()
-            idf = int(idf)
-            if dic.has_key((sub, rel, obj)):
-                if idf == dic[(sub, rel, obj)]['last']+1:
-                    dic[(sub, rel, obj)]['last'] += 1
-                else:
-                    first = dic[(sub, rel, obj)]['first']
-                    last = dic[(sub, rel, obj)]['last']
-                    dic[(sub, rel, obj)]['contiguous'].append((first, last))
-                    dic[(sub, rel, obj)]['first'] = idf
-                    dic[(sub, rel, obj)]['last'] = idf
-                    start_frames.append((idf, (sub, rel, obj)))
-            else:
-                dic[(sub, rel, obj)] = {'first':idf, 'last':idf, 'contiguous':[]}
-                start_frames.append((idf, (sub, rel, obj)))
-
-        for rel in dic:
-            first = dic[rel]['first']
-            last = dic[rel]['last']
-            dic[rel]['contiguous'].append((first, last))
-    logger.info('Found {} relations spread on {} lines of the input file.'.format(len(start_frames), i-1))
-    return dic, start_frames
-        
 
 def compress_relations(file_input, output=None, class_file='classes.cfg', rels_file='relations.cfg', keep_names=False):
     if not output:
         fname, _ = splitext(basename(file_input))
         output = join(dirname(file_input), fname+'_compressed.txt')
 
-    dcls = utils.load_classes(class_file)
-    logger.info('Loaded dictionary with {} objects.'.format(len(dcls)))
-    drels = utils.load_classes(rels_file)
-    logger.info('Loaded dictionary with {} relations.'.format(len(drels)))
-    drels_frames, start_frames = load_relations(file_input)
-    logger.info('Compressed to {} lines in output file.'.format(len(start_frames)))
+    # Load classes for objects from dict {0: 'rel0', 1: 'rel1'}
+    do = fh.ConfigFile(class_file).load_classes(cnames=True)
+    logger.info('Loaded dictionary with {} objects.'.format(len(do)))
+    dr = fh.ConfigFile(rels_file).load_classes(cnames=True)
+    logger.info('Loaded dictionary with {} relations.'.format(len(dr)))
+
+    df = fh.DecompressedFile(file_input)
+    dcomp = df.group_relations()
+    logger.info('Found {} relations spread on {} lines of the input file.'.format(len(df.start_frames), df.nb_lines-1))
+    logger.info('Compressed to {} lines in output file.'.format(len(df.start_frames)))
 
     logger.info('Saving output file...')
     with open(output, 'w') as fout:
         fout.write('Initial_frame-Final_frame-Subject-Relation-Object\n')
-        for _, key in sorted(start_frames):
-            start, end = drels_frames[key]['contiguous'].pop(0)
+        for _, key in sorted(df.start_frames):
+            start, end = dcomp[key]['contiguous'].pop(0)
             subj, rel, obj = key
             if keep_names:
                 fout.write('%d-%d-%s-%s-%s\n' % (start, end, subj, rel, obj))
             else:
-                fout.write('%d-%d-%d-%d-%d\n' % (start, end, dcls[subj], drels[rel], dcls[obj]))
+                fout.write('%d-%d-%d-%d-%d\n' % (start, end, do[subj], dr[rel], do[obj]))
     logger.info('File saved at: %s' % output)
-    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
