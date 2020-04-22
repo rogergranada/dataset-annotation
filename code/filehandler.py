7a9d3ac6433b1540aab4bda60618ee89914cc4a7
roger <roger.leitzke@gmail.com>
2020-04-22 18:38:32 -0300

    Update filehandler to support bbox

--- code/filehandler.py
+++ code/filehandler.py
@@ -140,6 +140,19 @@ class LisFile(FileHandler):
             self.fname = arr[4]
             yield arr
 
+    def _add_element(self, ids, pos):
+        if ids:
+            if pos:
+                objs = (self.idobj, self.x, self.y, self.w, self.h)
+            else:
+                objs = self.idobj
+        else:
+            if pos:
+                objs = (self.dobj, self.x, self.y. self.w, self.h)
+            else:
+                objs = self.dobj
+        return objs
+
     def objects_in_frame(self, ids=False, pos=False):
         last_id = -1
         objs = []
