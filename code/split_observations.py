f1c545ddc504db0fb56f02cb49777a4eb541285e
Roger Granada <roger.leitzke@gmail.com>
2020-07-22 01:18:10 -0300

    Split observations

--- code/split_observations.py
+++ code/split_observations.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-
+From a file containing Decompressed relations, this script generates 
+a set of files containing SPLITS percentage of the dataset. Thus, a
+file containing 100 frames, this script creates a folder `10/` containing
+a file with the same name of the input file, but containing only 10% of 
+the total of frames. Thus, the file contains 10 frames. The same is valid
+for `20/`, `30/`, `50/` and `70/`. The last containing 70% of the frames.
+ 
 """
 import logging
 logger = logging.getLogger(__name__)
@@ -18,6 +24,18 @@ import filehandler as fh
 SPLITS = [10, 20, 30, 50, 70]
 
 def save_file(fileoutput, sampling, idx2rels):
+    """ Save the selected sampling frames in the output file. 
+
+        Parameters:
+        -----------
+        fileoutput: string
+            path where the content is saved
+        sampling: array
+            list containing the selected indexes to be saved
+        idx2rels: dict
+            dictionary containing the id of the frame as key and
+            the relationship as value
+    """ 
     logger.info('Saving splitted observations at: {}'.format(fileoutput)) 
     with open(fileoutput, 'w') as fout:
         fout.write('Frame\tSubject\tRelation\tObject\n')
@@ -28,7 +46,18 @@ def save_file(fileoutput, sampling, idx2rels):
             id += 1
  
 
-def split_file(path, split, folders):
+def split_file(path, folders):
+    """ Create the set of splits for a single file.
+        
+        Parameters:
+        -----------
+        path: string
+            path to the input Decompressed file
+        folders: dict 
+            dictionary containing the id of the split as key
+            and the path to the folder where the split is 
+            saved as value.
+    """
     filerels = fh.DecompressedFile(path)
     idx2rels = {}
     with filerels as frels:
@@ -46,7 +75,18 @@ def split_file(path, split, folders):
         save_file(fileoutput, sampling, idx2rels)
 
 
-def split_data(folder_input, output=None):
+def split_from_folder(folder_input, output=None):
+    """ Read a folder containing Decompressed files and 
+        create a split dataset for each file.
+
+        Parameters:
+        -----------
+        folder_input: string
+            path to the folder containing Decompressed files
+        output: string (optional)
+            path to the folder where the files containing splits
+            are saved.
+    """
     if not output:
         output = join(folder_input, 'splits.tmp')
         output = fh.mkdir_from_file(output)
@@ -61,8 +101,7 @@ def split_data(folder_input, output=None):
     files = fh.FolderHandler(folder_input)
     for path in files:
         logger.info('Processing file: %s' % path)
-        split_file(path, split, folders)
-    
+        split_file(path, folders)
     logger.info('Finished successfully!')
 
 
@@ -72,4 +111,4 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', help='Plain text file', default=None)
     args = parser.parse_args()
     
-    split_data(args.input, args.output)
+    split_from_folder(args.input, args.output)
